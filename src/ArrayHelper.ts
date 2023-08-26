2e08cf27e49408d835be27d80718f045785d626c
Jeremy Zongker <jeremy@zongker.net>
2023-08-26 17:14:04 -0500

    Added appbase helpers

--- src/ArrayHelper.ts
+++ src/ArrayHelper.ts
@@ -1,9 +1,11 @@
+import { UniqueIdHelper } from "./UniqueIdHelper";
+
 export class ArrayHelper {
   static getIds(array: any[], propertyName: string) {
     const result: string[] = [];
     for (const item of array) {
       const id = item[propertyName]?.toString();
-      if (id && result.indexOf(id) === -1) result.push(id);
+      if (!UniqueIdHelper.isMissing(id) && result.indexOf(id) === -1) result.push(id);
     }
     return result;
   }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export { ArrayHelper } from "./ArrayHelper"
+
+export { ApiHelper } from "./ApiHelper";
+export { AppearanceHelper } from "./AppearanceHelper";
+export { ArrayHelper } from "./ArrayHelper";
+export { CurrencyHelper } from "./CurrencyHelper"
+export { DateHelper } from "./DateHelper";
+export { FileHelper } from "./FileHelper";
+export { PersonHelper } from "./PersonHelper"
+export { UserHelper } from "./UserHelper";
+export { UniqueIdHelper } from "./UniqueIdHelper";
+export { DonationHelper } from "./DonationHelper";
