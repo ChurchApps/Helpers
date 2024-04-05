05b1fec3dbe876f8921c040a66db38cba1d1437b
Jeremy Zongker <jeremy@zongker.net>
2024-04-05 17:21:57 -0500

    Updated doing interfaces

--- src/interfaces/Doing.ts
+++ src/interfaces/Doing.ts
@@ -4,9 +4,8 @@ export interface ConditionInterface { id?: string, conjunctionId?: string, field
 export interface ConjunctionInterface { id?: string, automationId?: string, parentId?: string, groupType?: string, conjunctions?: ConjunctionInterface[], conditions?: ConditionInterface[] }
 export interface TaskInterface { id?: string, taskNumber?: number, taskType?: string, dateCreated?: Date, dateClosed?: Date, associatedWithType?: string, associatedWithId?: string, associatedWithLabel?: string, createdByType?: string, createdById?: string, createdByLabel?: string, assignedToType?: string, assignedToId?: string, assignedToLabel?: string, title?: string, status?: string, automationId?: string, conversationId?: string }
 
-
 export interface PlanInterface { id?: string, churchId?:string, name?: string, ministryId?:string, serviceDate?: Date, notes?: string }
 export interface PositionInterface { id?: string, churchId?:string, planId?:string, categoryName?:string, name?: string, count?:number, groupId?:string }
-export interface AssignmentInterface { id?: string, churchId?:string, positionId?:string, personId?:string, status?:string }
+export interface AssignmentInterface { id?: string, churchId?:string, positionId?:string, personId?:string, status?:string, notified?:Date }
 export interface TimeInterface { id?: string, churchId?:string, planId?:string, displayName?:string, startTime?:Date, endTime?:Date, teams?:string, teamList?:string[] }
 export interface BlockoutDateInterface { id?: string; churchId?: string; personId?: string; startDate?: Date; endDate?: Date; }
