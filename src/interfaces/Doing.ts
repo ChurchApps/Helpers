8b144183c769929c15c423082f9681166148f442
Himali Malvawala <himalimalvawala@gmail.com>
2024-06-13 19:56:08 +0530

    add: data to tasks

--- src/interfaces/Doing.ts
+++ src/interfaces/Doing.ts
@@ -2,7 +2,7 @@ export interface ActionInterface { id?: string, automationId?: string, actionTyp
 export interface AutomationInterface { id?: string, title: string, recurs: string, active: boolean }
 export interface ConditionInterface { id?: string, conjunctionId?: string, field?: string, fieldData?: string, operator?: string, value?: string, label?: string }
 export interface ConjunctionInterface { id?: string, automationId?: string, parentId?: string, groupType?: string, conjunctions?: ConjunctionInterface[], conditions?: ConditionInterface[] }
-export interface TaskInterface { id?: string, taskNumber?: number, taskType?: string, dateCreated?: Date, dateClosed?: Date, associatedWithType?: string, associatedWithId?: string, associatedWithLabel?: string, createdByType?: string, createdById?: string, createdByLabel?: string, assignedToType?: string, assignedToId?: string, assignedToLabel?: string, title?: string, status?: string, automationId?: string, conversationId?: string }
+export interface TaskInterface { id?: string, taskNumber?: number, taskType?: string, dateCreated?: Date, dateClosed?: Date, associatedWithType?: string, associatedWithId?: string, associatedWithLabel?: string, createdByType?: string, createdById?: string, createdByLabel?: string, assignedToType?: string, assignedToId?: string, assignedToLabel?: string, title?: string, status?: string, automationId?: string, conversationId?: string, data?: string }
 
 export interface PlanInterface { id?: string, churchId?:string, name?: string, ministryId?:string, serviceDate?: Date, notes?: string }
 export interface PositionInterface { id?: string, churchId?:string, planId?:string, categoryName?:string, name?: string, count?:number, groupId?:string }
