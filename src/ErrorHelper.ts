13a4e11dec9626fc22c5012a8af82e27b2050834
Jeremy Zongker <jeremy@zongker.net>
2025-08-19 05:21:00 -0500

    Temporarily disabled error logging

--- src/ErrorHelper.ts
+++ src/ErrorHelper.ts
@@ -4,19 +4,20 @@ import { ApiHelper } from "./ApiHelper";
 
 export class ErrorHelper {
 
-  static getAppData:() => { churchId: string, userId: string, originUrl: string, application: string};
-  static customErrorHandler:(errorLog:ErrorLogInterface) => void;
+  static getAppData: () => { churchId: string, userId: string, originUrl: string, application: string };
+  static customErrorHandler: (errorLog: ErrorLogInterface) => void;
 
-  static init = (getAppData:() => ErrorAppDataInterface, customErrorHandler:(errorLog:ErrorLogInterface) => void) => {
+  static init = (getAppData: () => ErrorAppDataInterface, customErrorHandler: (errorLog: ErrorLogInterface) => void) => {
     ErrorHelper.getAppData = getAppData;
     ErrorHelper.customErrorHandler = customErrorHandler;
   }
 
-  static logError = (errorType:string, message:string, details:string) => {
-    if (this.getAppData)
-    {
+  static logError = (errorType: string, message: string, details: string) => {
+
+
+    if (this.getAppData) {
       const data = this.getAppData();
-      const log:ErrorLogInterface = {
+      const log: ErrorLogInterface = {
         application: data.application,
         errorTime: new Date(),
         userId: data.userId,
@@ -27,10 +28,12 @@ export class ErrorHelper {
         details: details
       }
 
-      if (log.errorType==="401" && log.message.indexOf("/users/login")>-1) return;
-      ApiHelper.postAnonymous("/errors", [log], "MembershipApi");
+      if (log.errorType === "401" && log.message.indexOf("/users/login") > -1) return;
+      //temporarily disabled error logging
+      //ApiHelper.postAnonymous("/errors", [log], "MembershipApi");
       if (ErrorHelper.customErrorHandler) ErrorHelper.customErrorHandler(log);
     }
   }
+
 
 }
